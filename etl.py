b5315b34081316a05a3a80a10f3defde49a267a9
ifeadikanwa <ezeifyyy@gmail.com>
2026-04-09 06:03:37 -0400

    transformed extracted data

--- etl.py
+++ etl.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 #define my api endpoint
 url = "https://dummyjson.com/products"
@@ -13,3 +14,26 @@ data = response.json()
 #list of dicts
 products = data["products"]
 
+
+
+# convert products list into a dataframe
+df = pd.DataFrame(products)
+
+# clean our data by selecting only relevant columns
+df = df[["id",
+         "title",
+         "price",
+         "discountPercentage",
+         "category",
+         "rating",
+         "stock",
+         ]]
+
+print(df.head())
+
+# transform our data
+# add didscounted price column
+df["discounted_price"] = df["price"] * (1 - (df["discountPercentage"]/100))
+
+print(df.head())
+print(df.info())
